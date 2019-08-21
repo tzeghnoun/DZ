4a2e571b9da18829b258f04992602193a9090088
Tewfik <zeghnoun@yahoo.fr>
2019-08-21 13:55:38 +0100

    Updated the script and the animated map of the French nuclear test in Algeria between 1960 & 1966

--- TidyTuesday_French_nuclear_tests_in_Algeria.R
+++ TidyTuesday_French_nuclear_tests_in_Algeria.R
@@ -19,28 +19,28 @@ nuclear_explosions[, my_date := ymd(date_long)]
 # Create an animated map of the French nuclear tests in Algeria
 dz_map <- nuclear_explosions[region %like% "ALG"] %>%
   ggplot(aes(longitude, latitude)) +
-  borders("world", regions = "Algeria") +
-  geom_point(size = 10, alpha = .5, color = "green") +
+  borders("world", regions = "Algeria", colour = "#c2c2c2", fill = "#b58b43") +
+  geom_jitter(size = 15, alpha = .5, color = "green") +
+  geom_jitter(size = 10, alpha = .7, color = "red") +
   geom_text_repel(mapping = aes(longitude, latitude, 
                                 label = as.character(my_date)), nudge_x = 1, 
-                  color = "#948f8f", size = 7) +
-  scale_color_manual(values = mycolors) +
-  annotate(geom="text", x=-1, y= 28, label="REGGANE", color="#6D7C83", 
+                  color = "#665840", size = 7) +
+  annotate(geom="text", x=-1, y= 28, label="REGGANE", color="#334b57", 
            size=10, hjust=0.5,vjust=0, fontface="bold") +
-  annotate(geom="text", x=7, y= 27, label="IN ECKER", color="#6D7C83", 
+  annotate(geom="text", x=7, y= 27, label="IN ECKER", color="#334b57", 
            size=10, hjust=0.5,vjust=0, fontface="bold") +
   transition_states(my_date,1,1) +
   shadow_mark(past=TRUE) +
   labs(
     title = "French nuclear tests in Algeria",
-    subtitle = 'Time period : 1960 - 1966',
+    subtitle = '17 explosions between 1960 & 1966',
     caption = "Source : Stockholm International Peace Research Institute"
   ) +
-  dark_mode(theme_void(base_size = 22))
+  dark_mode(theme_void(base_size = 28))
 
 options(gganimate.dev_args = list(width = 900, height = 900))
-p_ani <- animate(dz_map, nframes = 90, fps=3, detail = 1)
+p_ani <- animate(dz_map, nframes = 50, fps=3, detail = 1)
 p_ani
 
 # Save the gif
-anim_save("map_ani.gif")
+anim_save("figs/map_ani.gif")
