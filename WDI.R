d975d01c2228a9a739c8dca60faa3d0d3cf24fbb
Tewfik <zeghnoun@yahoo.fr>
2019-09-22 12:09:31 +0100

    Added plot saving lines to the script

--- WDI.R
+++ WDI.R
@@ -7,10 +7,9 @@ library(ggrepel)
 dz_plot <- fread('data/wdi_dz.csv', header = TRUE)
 
 # Labor force, female (% of total labor force)		
-dz_plot <- dz[indicator_code == 'SL.TLF.TOTL.FE.ZS'] 
-dz_plot %>% 
+dz_plot[indicator_code == 'SL.TLF.TOTL.FE.ZS'] %>% 
   ggplot() +
-  aes(year, value, color = indicator_code) +
+  aes(year, value) +
   geom_line() +
   labs(
     title = "Labor force, female (% of total labor force)",
@@ -37,10 +36,10 @@ dz_plot %>%
         strip.text = element_text(size = 13, face = "bold"),
         legend.position = 'none'
   ) 
-
+ggsave("labor_force_fe.png", path = "figs/", width = 12, height = 8)
 
 # Adolescent fertility rate (births per 1,000 women ages 15-19)	
-dz[indicator_code == 'SP.ADO.TFRT'] %>% 
+dz_plot[indicator_code == 'SP.ADO.TFRT'] %>% 
   ggplot() +
   aes(year, value) +
   geom_line() +
@@ -69,8 +68,10 @@ dz[indicator_code == 'SP.ADO.TFRT'] %>%
         strip.text = element_text(size = 13, face = "bold"),
         legend.position = 'none'
   ) 
+ggsave("adolescent_fertility_rate.png", path = "figs/", width = 12, height = 8)
+
 # CO2 emissions (kg per 2010 US$ of GDP)	
-dz[indicator_code %like% 'EN.ATM.CO2E.KD.GD'] %>% 
+dz_plot[indicator_code %like% 'EN.ATM.CO2E.KD.GD'] %>% 
   ggplot() +
   aes(year, value) +
   geom_line() +
@@ -99,9 +100,10 @@ dz[indicator_code %like% 'EN.ATM.CO2E.KD.GD'] %>%
         strip.text = element_text(size = 13, face = "bold"),
         legend.position = 'none'
   ) 
+ggsave("co2_emissions.png", path = "figs/", width = 12, height = 8)
 
 # Final consumption expenditure (% of GDP)	
-dz[indicator_code %like% 'NE.CON.TOTL.ZS'] %>% 
+dz_plot[indicator_code %like% 'NE.CON.TOTL.ZS'] %>% 
   ggplot() +
   aes(year, value) +
   geom_line() +
@@ -130,8 +132,10 @@ dz[indicator_code %like% 'NE.CON.TOTL.ZS'] %>%
         strip.text = element_text(size = 13, face = "bold"),
         legend.position = 'none'
   ) 
+ggsave("final_consumption_expend.png", path = "figs/", width = 12, height = 8)
+
 # GDP per capita (constant 2010 US$)
-dz[indicator_code == 'NY.GDP.PCAP.KD'] %>% 
+dz_plot[indicator_code == 'NY.GDP.PCAP.KD'] %>% 
   ggplot() +
   aes(year, value) +
   geom_line() +
@@ -164,8 +168,10 @@ dz[indicator_code == 'NY.GDP.PCAP.KD'] %>%
         strip.text = element_text(size = 13, face = "bold"),
         legend.position = 'none'
   ) 
+ggsave("gdp_per_capita.png", path = "figs/", width = 12, height = 8)
+
 # Imports of goods and services (% of GDP)
-dz[indicator_code == 'NE.IMP.GNFS.ZS'] %>% 
+dz_plot[indicator_code == 'NE.IMP.GNFS.ZS'] %>% 
   ggplot() +
   aes(year, value) +
   geom_line() +
@@ -196,9 +202,10 @@ dz[indicator_code == 'NE.IMP.GNFS.ZS'] %>%
         strip.text = element_text(size = 13, face = "bold"),
         legend.position = 'none'
   ) 
+ggsave("import_goods_services.png", path = "figs/", width = 12, height = 8)
 
 # Mobile cellular subscriptions (per 100 people)
-dz[indicator_code == 'IT.CEL.SETS.P2' & year > 2000] %>% 
+dz_plot[indicator_code == 'IT.CEL.SETS.P2' & year > 2000] %>% 
   ggplot() +
   aes(year, value) +
   geom_line() +
@@ -229,9 +236,10 @@ dz[indicator_code == 'IT.CEL.SETS.P2' & year > 2000] %>%
         strip.text = element_text(size = 13, face = "bold"),
         legend.position = 'none'
   )
+ggsave("mobile_cellular_subsc.png", path = "figs/", width = 12, height = 8)
 
 # Population growth (annual %)
-dz[indicator_code == 'SP.POP.GROW'] %>% 
+dz_plot[indicator_code == 'SP.POP.GROW'] %>% 
   ggplot() +
   aes(year, value) +
   geom_line() +
@@ -262,3 +270,4 @@ dz[indicator_code == 'SP.POP.GROW'] %>%
         strip.text = element_text(size = 13, face = "bold"),
         legend.position = 'none'
   )
+ggsave("population_growth.png", path = "figs/", width = 12, height = 8)
