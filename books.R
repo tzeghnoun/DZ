620fe256c9338e07db1e8d76ae735e96daf8a3a6
Tewfik <zeghnoun@yahoo.fr>
2019-10-23 09:29:18 +0100

    Updated the script and cleaned the dataset

--- books.R
+++ books.R
@@ -1,9 +1,12 @@
 library(data.table)
 library(rvest)
-library(robotstxt)
+# library(robotstxt)
 library(tidyverse)
-library(stringi)
+# library(stringi)
 library(lubridate)
+library(ggforce)
+library(gridExtra)
+library(ggrepel)
 
 my_col <- c("#C3D7A4", "#28AADC", "#24576D", "#248E84",
             "#FFDB6D", "#C4961A", "#F4EDCA", "#D16103",
@@ -12,30 +15,27 @@ my_col <- c("#C3D7A4", "#28AADC", "#24576D", "#248E84",
             "#D95F02", "#7570B3", "#E7298A", "#66A61E",
             "#E6AB02", "#A6761D", "#666666")
 
-books <- fread('data/berzakh_catalogue.csv', header = TRUE,
-               col.names = c('id', 'date', 'auteur', 'titre', 'resume'))
- 
-books <- books[, date := gsub('Aout', 'Août', date)
-               ][, date := dmy(paste0('01 ', date), locale = 'French')]
 
-data_plot <- books[, .N, by = .(year(date), months(date), auteur, titre)]
+books <- fread('data/berzakh_catalogue.csv', header = TRUE)
+books <- books[, .(date, auteur, titre, resume, genre)]
+
+data_plot <- books[, .N, by = .(year(date), months(date), auteur, titre, genre)]
 data_plot <- data_plot[year != 2019
                        ][, period := ifelse(year > 2011, '2012-2018', '2002-2011')]
 data_plot$months <- factor(data_plot$months, 
-                           levels = c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
-
+                           levels = c("December", "January", "February", "March", "April", 
+                                      "May", "June", "July", "August", "September", "October", "November"))
+# Bar plot
 data_plot %>% ggplot() +
-  aes(months, N, fill = period) +
+  aes(months, N, fill = genre) +
   geom_bar(stat = 'identity') +
-  scale_x_discrete( name = "Month" ) +
-  # scale_x_date(date_breaks = 'month', date_labels = "%b") +
-  scale_fill_manual(values = sample(my_col)) +
+  scale_fill_manual(values = my_col, name = 'Genre') +
   labs(
     title = 'Number of book titles published by the Algerian book publisher Barzakh',
-    subtitle = 'Time period : 2002 - 2019',
+    subtitle = 'Time period : 2002 - 2019\n',
     x = '',
     y = 'Number of books published',
-    caption = '\nhttp://www.editions-barzakh.com/catalogue'
+    caption = '\n'
   ) +
   coord_flip() +
   theme(text = element_text(color = "gray20"),
@@ -48,9 +48,176 @@ data_plot %>% ggplot() +
         axis.line.y = element_line(color = "gray40", size = 0.5),
         panel.grid.major = element_line(color = "white", size = 0.5),
         panel.grid.major.x = element_blank(),
-        panel.background = element_rect('white'))
+        panel.background = element_rect('white')) +
+  facet_wrap(~period)
 
 ggsave('figs/barzakh_published_books.png', width = 14, height = 6, dpi = 320)
+
+# Scater plot
+data_scater <- data_plot[, .N, by = .(year, months, genre, period)
+                         ][, trend := ifelse(months %in% c('December', 'January') & period == '2002-2011', 'a', 
+                                             ifelse(months %in% c('September', 'October') & period == '2012-2018', 'b', NA))] 
+data_scater %>% ggplot() +
+  aes(year, months, color = genre, size = N) +
+  geom_jitter() +
+  scale_color_manual(values = my_col) +
+  labs(
+    title = '',
+    subtitle = '\n',
+    x = '',
+    y = '',
+    caption = '\nhttp://www.editions-barzakh.com/catalogue'
+  ) +
+  # coord_flip() +
+  theme(text = element_text(color = "gray20"),
+        legend.text = element_text(size = 11, color = "gray10"),
+        axis.text = element_text(face = "italic"),
+        # axis.title.x = element_text(hjust = -1), # move title away from axis
+        axis.title.y = element_text(vjust = 2), # move away for axis
+        axis.ticks.y = element_blank(), # element_blank() is how we remove elements
+        axis.line = element_line(color = "gray40", size = 0.5),
+        axis.line.y = element_line(color = "gray40", size = 0.5),
+        panel.grid.major = element_line(color = "white", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_rect('white'),
+        legend.position = 'none') +
+  facet_wrap(~period, scales = 'free_x')
+
+p <- gridExtra::grid.arrange(p1, p2, ncol = 2)
+
+ggsave('figs/P2_barzakh_published_books.png', plot = p, width = 14, height = 6, dpi = 320)
+
+# Top auteur more than 3 publication
+top_auteur <- data_plot[!is.na(auteur), .N, by = .(auteur, genre, period)][order(-N)] 
+top_auteur$csum <- ave(top_auteur$N, top_auteur$auteur, FUN=cumsum)
+
+top_auteur4 <- top_auteur[csum > 4]
+
+auteurs <- top_auteur[csum > 1]$auteur
+
+ top_auteur[auteur %in% auteurs] %>%
+  ggplot() +
+  aes(reorder(auteur, csum), N, fill = genre) +
+  geom_bar(stat = 'identity') +
+  scale_fill_manual(values = my_col, name = 'Period') +
+  scale_y_continuous(breaks = seq(0, 10, 1)) +
+  labs(
+    title = 'Top published authors',
+    subtitle = 'Published more than ones\n',
+    x = '',
+    y = '',
+    caption = '\n\n'
+  ) +
+  coord_flip() +
+  theme(text = element_text(color = "gray20"),
+        legend.text = element_text(size = 11, color = "gray10"),
+        axis.text = element_text(face = "italic"),
+        # axis.title.x = element_text(hjust = -1), # move title away from axis
+        axis.title.y = element_text(vjust = 2), # move away for axis
+        axis.ticks.y = element_blank(), # element_blank() is how we remove elements
+        axis.line = element_line(color = "gray40", size = 0.5),
+        axis.line.y = element_line(color = "gray40", size = 0.5),
+        panel.grid.major = element_line(color = "white", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_rect('white')) +
+   facet_wrap(~period)
+  
+
+
+data_plot %>% ggplot() +
+  aes(year, months, color = genre) +
+  geom_point() +
+  scale_color_manual(values = my_col) +
+  geom_text(data = data_plot[auteur %in% top_auteur4$auteur],
+            mapping = aes(x= year, y = months, label = auteur), vjust = -1) +
+  labs(
+    title = 'Authors with more than 4 publications',
+    subtitle = '\n',
+    x = '',
+    y = '',
+    caption = '\nhttp://www.editions-barzakh.com/catalogue\n'
+  ) +
+  # coord_flip() +
+  theme(text = element_text(color = "gray20"),
+        legend.text = element_text(size = 11, color = "gray10"),
+        axis.text = element_text(face = "italic"),
+        # axis.title.x = element_text(hjust = -1), # move title away from axis
+        axis.title.y = element_text(vjust = 2), # move away for axis
+        axis.ticks.y = element_blank(), # element_blank() is how we remove elements
+        axis.line = element_line(color = "gray40", size = 0.5),
+        axis.line.y = element_line(color = "gray40", size = 0.5),
+        panel.grid.major = element_line(color = "white", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_rect('white'),
+        legend.position = 'none') +
+  facet_wrap(~period, scales = 'free_x')
+
+pp <- gridExtra::grid.arrange(p3, p4, ncol = 2)
+
+ggsave('figs/P3_barzakh_published_books.png', plot = pp, width = 14, height = 6, dpi = 320)
+
+# Genre
+data_plot %>% ggplot() +
+  aes(months, N, fill = genre) +
+  geom_bar(stat = 'identity') +
+  scale_fill_manual(values = my_col, name = 'Genre') +
+  labs(
+    title = 'Number of book titles published by the Algerian book publisher Barzakh',
+    subtitle = 'Time period : 2002 - 2019\n',
+    x = '',
+    y = 'Number of books published',
+    caption = '\n'
+  ) +
+  coord_flip() +
+  theme(text = element_text(color = "gray20"),
+        legend.text = element_text(size = 11, color = "gray10"),
+        axis.text = element_text(face = "italic"),
+        # axis.title.x = element_text(hjust = -1), # move title away from axis
+        axis.title.y = element_text(vjust = 2), # move away for axis
+        axis.ticks.y = element_blank(), # element_blank() is how we remove elements
+        axis.line = element_line(color = "gray40", size = 0.5),
+        axis.line.y = element_line(color = "gray40", size = 0.5),
+        panel.grid.major = element_line(color = "white", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_rect('white')) +
+  facet_wrap(~period)
+
+
+data_plot %>% ggplot() +
+  aes(year, months, color = genre) +
+  geom_point() +
+  scale_color_manual(values = my_col) +
+  geom_text_repel(data = data_plot[auteur %in% top_auteur4$auteur],
+                  mapping = aes(x= year, y = months, label = auteur), vjust = -1) +
+  labs(
+    title = 'Authors with more than 4 publications',
+    subtitle = '\n',
+    x = '',
+    y = '',
+    caption = '\nhttp://www.editions-barzakh.com/catalogue\n'
+  ) +
+  # coord_flip() +
+  theme(text = element_text(color = "gray20"),
+        legend.text = element_text(size = 11, color = "gray10"),
+        axis.text = element_text(face = "italic"),
+        # axis.title.x = element_text(hjust = -1), # move title away from axis
+        axis.title.y = element_text(vjust = 2), # move away for axis
+        axis.ticks.y = element_blank(), # element_blank() is how we remove elements
+        axis.line = element_line(color = "gray40", size = 0.5),
+        axis.line.y = element_line(color = "gray40", size = 0.5),
+        panel.grid.major = element_line(color = "white", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_rect('white'),
+        legend.position = 'none') 
+
+
+
+
+
+
+
+
+
 
 data_plot[year(date) < 2019 & year(date) > 2011, .(.N/7), by = .(months(date))
           ][order(-V1)
@@ -87,12 +254,13 @@ data_plot[year(date) < 2012 & year(date) > 2002, .(.N/10), by = .(months(date))
 # titre_data <- lapply(1:20, titre)
 # titre_data_unlist <- unlist(titre_data)
 # 
-# # Need to change the urls to get the resume & the date
+# # # Need to change the urls to get the resume & the date
 # url <- 'http://www.editions-barzakh.com/catalogue/'
 # # Remove the french accent in the title list
-# titre_url <- stri_trans_general(str = titre_data_unlist, id = "Latin-ASCII")
+# titre_url <- stri_trans_general(str = books$titre, id = "Latin-ASCII")
 # 
 # titre_url <- gsub("\\s$", "", titre_url)
+# titre_url <- gsub("\\.+$", "", titre_url)
 # titre_url <- gsub(" ", "-", titre_url)
 # titre_url <- gsub(",", "", titre_url)
 # titre_url <- gsub("'", "", titre_url)
@@ -101,7 +269,7 @@ data_plot[year(date) < 2012 & year(date) > 2002, .(.N/10), by = .(months(date))
 # titre_url <- tolower(titre_url)
 # 
 # urls <- map2_chr(url, titre_url, function(x, y) {paste0(x, y)})
-# 
+# # 
 # 
 # resume_data <- lapply(1:198, resume)
 # # Check the links that doesn't work
@@ -137,8 +305,21 @@ data_plot[year(date) < 2012 & year(date) > 2002, .(.N/10), by = .(months(date))
 # 
 # date_data_unlist <- unlist(date_data)
 # 
-# # Concatenate the vectors to create a DT
-# berzakh_catalogue <- data.table(date_data_unlist, auteur_data_unlist, titre_data_unlist, resume_data_unlist)
+# # # Concatenate the vectors to create a DT
+# # berzakh_catalogue <- data.table(date_data_unlist, auteur_data_unlist, titre_data_unlist, resume_data_unlist)
+# # 
+# genre_data <- lapply(1:198, function(x) {
+#     tryCatch(
+#       urls[x] %>%
+#         read_html() %>%
+#         html_nodes(".filter-link") %>%
+#         html_attr("href"),
+#       error = function(e){NA}    # a function that returns NA regardless of what it's passed
+#     )
+#   })
 # 
-# write.csv(berzakh_catalogue, 'data/berzakh_catalogue.csv')
-
+# genre_data_unlist <- unlist(genre_data)
+# genre_data_unlist <- str_sub(genre_data_unlist, 19)
+# 
+# books <- books[, genre := genre_data_unlist]
+write.csv(books, 'data/berzakh_catalogue.csv')
